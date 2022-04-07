81e4228f608f1ad9d502e1c21e608ef9d845abb5
Corey Yates <coreyyates@Coreys-iMac.local>
2022-04-07 15:33:12 -0400

    carousel images load from NASA api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,22 @@
-// $(document).ready(function () {
-//     var imgObjArray = []
-//   var imageContainer = $(".carousel-inner");
-//   fetch("https://images-api.nasa.gov/search?q=hubble&page=1", {
-//     api_key: "WSXS6UpIdCzVPyL4krzd37fhXgvuFfWGiAdgtNgh",
-//   })
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//         for (var i = 0; i < 10; i++) {
-//           var imgObj = {
-//               img_path: data.collection.items[i].links[0].href,
-//               status: "",
-//               className: "d-block"
-//           }
-//           imgObjArray.push(imgObj)
-        
-//           // imgDiv.addClass("carousel-item");
-//           // imgDiv.addClass(imgObj.className)
-//           // imgDiv.attr("data-bs-interval", "10000");
-//           // imgEL.attr("src",imgObj.img_path)
-//           // imgDiv.addClass("d-block w-100")
-//           // imageContainer.append(imgDiv.append(imgEL))
-//         }
-//         buildImageCarousel(imgObjArray)
-//     //   imgDiv.addClass("d-block w-100")
-//     });
-// });
+$(document).ready(function () {
+  fetch("https://images-api.nasa.gov/search?q=hubble&page=1", {
+    api_key: "WSXS6UpIdCzVPyL4krzd37fhXgvuFfWGiAdgtNgh",
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      var imgElArray = document.querySelectorAll("a")
+        for (var i = 0; i < imgElArray.length; i++) {
+          var targetNum = imgElArray[i].getAttribute("data-index")
+          console.log(targetNum)
+          console.log(imgElArray[i].children[0])
+          imgElArray[targetNum].children[0].setAttribute("src", data.collection.items[targetNum].links[0].href)
+        }
+  
+    });
+});
 
 // function buildImageCarousel(imgArray) {
 //     var imgDiv = $("<div>").addClass("carousel-item");
